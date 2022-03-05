383a3d7b15b7de7a89288ecc4ebf2dcea1fcbfb8
Fernando Ochoa O <fochoao@pm.me>
2022-03-05 01:56:25 -0600

    Initial commit

---
+++ test/testutil/output_helpers.d
@@ -0,0 +1,7 @@
+output_helpers.c
+Nota: inclusión del archivo: C:\cpython-3.11\externals\openssl-1.1.1m\test\testutil\output.h
+Nota: inclusión del archivo:  C:\VSStudio\2022\Enterprise\VC\Tools\MSVC\14.31.31103\include\stdarg.h
+Nota: inclusión del archivo:   C:\VSStudio\2022\Enterprise\VC\Tools\MSVC\14.31.31103\include\vcruntime.h
+Nota: inclusión del archivo:    C:\VSStudio\2022\Enterprise\VC\Tools\MSVC\14.31.31103\include\sal.h
+Nota: inclusión del archivo:     C:\VSStudio\2022\Enterprise\VC\Tools\MSVC\14.31.31103\include\concurrencysal.h
+Nota: inclusión del archivo:    C:\VSStudio\2022\Enterprise\VC\Tools\MSVC\14.31.31103\include\vadefs.h
